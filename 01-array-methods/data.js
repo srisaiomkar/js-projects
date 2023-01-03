924dd51a53bb0b8f0993c9dd03e9e534b012892f
Sri Sai Omkar Akilla <srisaiomkar@gmail.com>
2023-01-03 10:46:05 -0500

    array methods

---
+++ 01-array-methods/data.js
@@ -0,0 +1,32 @@
+const students = [
+  {
+    id: 1,
+    name: "John",
+    score: 80,
+    favouriteSubject: "math",
+  },
+  {
+    id: 2,
+    name: "Bob",
+    score: 70,
+    favouriteSubject: "english",
+  },
+  {
+    id: 1,
+    name: "Peter",
+    score: 90,
+    favouriteSubject: "science",
+  },
+  {
+    id: 1,
+    name: "Mary",
+    score: 70,
+    favouriteSubject: "social",
+  },
+  {
+    id: 1,
+    name: "Susan",
+    score: 40,
+    favouriteSubject: "math",
+  },
+];
